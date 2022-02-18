b03690c84c4a40975a484661f691949b96dd67a3
James Fitzgerald <fitzosapian@gmail.com>
2022-02-18 17:30:12 +0000

    first commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById("weatherSubmit").addEventListener("click", function (eve
     const value = Math.floor(Math.random() * 556);;
     event.preventDefault();
     console.log(value);
-    const url = "http://api.alquran.cloud/v1/ayah/"+value+"/en.asad";
+    const url = "http://api.alquran.cloud/v1/ayah/" + value + "/en.asad";
     console.log(url);
     fetch(url)
         .then(function (response) {
@@ -11,10 +11,10 @@ document.getElementById("weatherSubmit").addEventListener("click", function (eve
             console.log(json.data.text);
             let results = "";
             results += "<h2>Bot1 says:</h2>";
-            results += "<p>"+json.data.text+"</p>";
+            results += "<p>" + json.data.text + "</p>";
             document.getElementById("bot1res").innerHTML = results;
         });
-    
+
     const url2 = "https://poetrydb.org/random";
     console.log(url2);
     fetch(url2)
@@ -24,13 +24,26 @@ document.getElementById("weatherSubmit").addEventListener("click", function (eve
             console.log(json[0].lines[0]);
             let results = "";
             results += "<h2>Bot2 says:</h2>";
-            results +="<p>";
-            for(let i =0; i <20; i++){
-                if(i < json[0].lines.length){
-                    results += json[0].lines[i];
+
+            for (let i = 0; i < 20; i++) {
+                if (i < json[0].lines.length) {
+                    results += "<p>" + json[0].lines[i] + "</p>";
                 }
             }
-            results += "</p>";
             document.getElementById("bot2res").innerHTML = results;
+        });
+
+    const url3 = "https://random.dog/woof.json";
+    console.log(url3);
+    fetch(url3)
+        .then(function (response) {
+            return response.json();
+        }).then(function (json) {
+            console.log(json);
+            let results = "";
+            results += "<h2>Bot3 says:</h2>";
+
+            results += "<img src=\""+json.url+"\" style =\"width:100%\"/>";
+            document.getElementById("bot3res").innerHTML = results;
         });
 });
